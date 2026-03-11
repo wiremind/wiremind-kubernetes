d14d1dc22e14430411d20928b51b90b4d3a30183
Cédric de Saint Martin <cdesaintmartin@wiremind.io>
2026-03-11 13:28:34 +0100

    fix(client): skip pretty on custom GET reads
    Prevent the client wrapper from forwarding shared read kwargs to
    CustomObjectsApi methods whose generated signatures reject them.
    
    - exclude `pretty` from custom-object GET-like methods only
    - keep pretty output on list methods that still support it
    - add regression tests for both behaviors and document why
    
    Subagents-used: Explore
    Skills-used: git-commit
    
    Planned-by: GitHub Copilot / GPT-5.4 / 0.0
    Built-by: GitHub Copilot / GPT-5.4 / 0.0
    Signed-off-by: Cédric de Saint Martin <cdesaintmartin@wiremind.io>

--- src/wiremind_kubernetes/kubernetes_client_additional_arguments.py
+++ src/wiremind_kubernetes/kubernetes_client_additional_arguments.py
@@ -13,12 +13,12 @@ class ClientWithArguments:
     client: Any
     read_additional_arguments: Dict[str, Any]
     additional_arguments: Dict[str, Any]
+    read_argument_exclusions: dict[str, set[str]]
 
     def __init__(self, client: Any, dry_run: bool = False, pretty: bool = True):
         self.client = client()  # like kubernetes.client.CoreV1Api
         self.read_additional_arguments = {}
-        # Only add it when its true because we set pretty client wide,
-        # read_cluster_custom_object which accepts it will not have it set, but it's ok for now.
+        self.read_argument_exclusions = {}
         if pretty:
             self.read_additional_arguments["pretty"] = pretty
         # Every request, either read or write, will have those arguments added
@@ -26,6 +26,13 @@ class ClientWithArguments:
         if dry_run:
             # Dry run, in kube API, is not true or false, but either dry_run: All or not defined.
             self.additional_arguments["dry_run"] = "All"
+
+    def get_read_additional_arguments(self, attr: str) -> Dict[str, Any]:
+        read_additional_arguments = self.read_additional_arguments.copy()
+        # Some generated client read methods do not accept every shared read kwarg.
+        for argument_name in self.read_argument_exclusions.get(attr, set()):
+            read_additional_arguments.pop(argument_name, None)
+        return read_additional_arguments
 
     def __getattr__(self, attr: str) -> Any:
         original_attr = getattr(self.client, attr)
@@ -43,56 +50,107 @@ class ClientWithArguments:
             if is_write_function:
                 kwargs.update(self.additional_arguments)
             else:  # A read function
-                kwargs.update(self.read_additional_arguments)
+                kwargs.update(self.get_read_additional_arguments(attr))
             return original_attr(*args, **kwargs)
 
         return fn
 
 
 class CoreV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.CoreV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.CoreV1Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class AppV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.AppsV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.AppsV1Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class BatchV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.BatchV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.BatchV1Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class AutoscalingV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.AutoscalingV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.AutoscalingV1Api, dry_run=dry_run, pretty=pretty
+        )
+
 
 class AutoscalingV2ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.AutoscalingV2Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.AutoscalingV2Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class CustomObjectsApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.CustomObjectsApi, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.CustomObjectsApi, dry_run=dry_run, pretty=pretty
+        )
+        # Kubernetes custom-object GET-like methods reject `pretty`, while list methods still accept it.
+        self.read_argument_exclusions = {
+            "get_api_resources": {"pretty"},
+            "get_cluster_custom_object": {"pretty"},
+            "get_cluster_custom_object_scale": {"pretty"},
+            "get_cluster_custom_object_status": {"pretty"},
+            "get_namespaced_custom_object": {"pretty"},
+            "get_namespaced_custom_object_scale": {"pretty"},
+            "get_namespaced_custom_object_status": {"pretty"},
+        }
 
 
 class RbacAuthorizationV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.RbacAuthorizationV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.RbacAuthorizationV1Api,
+            dry_run=dry_run,
+            pretty=pretty,
+        )
 
 
 class NetworkingV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.NetworkingV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.NetworkingV1Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class StorageV1ApiWithArguments(ClientWithArguments):
-    def __init__(self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.StorageV1Api, dry_run=dry_run, pretty=pretty)
+    def __init__(
+        self, *args: Any, dry_run: bool = False, pretty: bool = False, **kwargs: Any
+    ) -> None:
+        super().__init__(
+            client=kubernetes.client.StorageV1Api, dry_run=dry_run, pretty=pretty
+        )
 
 
 class AdmissionregistrationV1ApiWithArguments(ClientWithArguments):
     def __init__(self, *args: Any, dry_run: bool = False, **kwargs: Any) -> None:
-        super().__init__(client=kubernetes.client.AdmissionregistrationV1Api, dry_run=dry_run)
+        super().__init__(
+            client=kubernetes.client.AdmissionregistrationV1Api, dry_run=dry_run
+        )
--- src/wiremind_kubernetes/tests/unit_tests/kubernetes_client_additional_arguments_test.py
+++ src/wiremind_kubernetes/tests/unit_tests/kubernetes_client_additional_arguments_test.py
@@ -1,14 +1,21 @@
+import pytest
 from pytest_mock import MockerFixture
 
 import wiremind_kubernetes.kubernetes_helper
 
 
-def test_kubernetes_client_additional_arguments_core_v1_api(mocker: MockerFixture) -> None:
+def test_kubernetes_client_additional_arguments_core_v1_api(
+    mocker: MockerFixture,
+) -> None:
     """
     Test that we add mandatory args to each function call of kubernetes client
     """
-    mocked_read_namespaced_pod = mocker.patch("kubernetes.client.CoreV1Api.read_namespaced_pod")
-    mocked_create_namespaced_pod = mocker.patch("kubernetes.client.CoreV1Api.create_namespaced_pod")
+    mocked_read_namespaced_pod = mocker.patch(
+        "kubernetes.client.CoreV1Api.read_namespaced_pod"
+    )
+    mocked_create_namespaced_pod = mocker.patch(
+        "kubernetes.client.CoreV1Api.create_namespaced_pod"
+    )
 
     kubernetes_helper = wiremind_kubernetes.kubernetes_helper.KubernetesHelper(
         dry_run=True, should_load_kubernetes_config=False
@@ -18,15 +25,23 @@ def test_kubernetes_client_additional_arguments_core_v1_api(mocker: MockerFixtur
     mocked_read_namespaced_pod.assert_called_once_with("foo", "bar", pretty=True)
 
     kubernetes_helper.client_corev1_api.create_namespaced_pod("foo", "bar")
-    mocked_create_namespaced_pod.assert_called_once_with("foo", "bar", pretty=True, dry_run="All")
+    mocked_create_namespaced_pod.assert_called_once_with(
+        "foo", "bar", pretty=True, dry_run="All"
+    )
 
 
-def test_kubernetes_client_additional_arguments_disabled_core_v1_api(mocker: MockerFixture) -> None:
+def test_kubernetes_client_additional_arguments_disabled_core_v1_api(
+    mocker: MockerFixture,
+) -> None:
     """
     Test that we do not add args to each function call of kubernetes client
     """
-    mocked_read_namespaced_pod = mocker.patch("kubernetes.client.CoreV1Api.read_namespaced_pod")
-    mocked_create_namespaced_pod = mocker.patch("kubernetes.client.CoreV1Api.create_namespaced_pod")
+    mocked_read_namespaced_pod = mocker.patch(
+        "kubernetes.client.CoreV1Api.read_namespaced_pod"
+    )
+    mocked_create_namespaced_pod = mocker.patch(
+        "kubernetes.client.CoreV1Api.create_namespaced_pod"
+    )
 
     kubernetes_helper = wiremind_kubernetes.kubernetes_helper.KubernetesHelper(
         dry_run=True, pretty=False, should_load_kubernetes_config=False
@@ -37,3 +52,58 @@ def test_kubernetes_client_additional_arguments_disabled_core_v1_api(mocker: Moc
 
     kubernetes_helper.client_corev1_api.create_namespaced_pod("foo", "bar")
     mocked_create_namespaced_pod.assert_called_once_with("foo", "bar", dry_run="All")
+
+
+@pytest.mark.parametrize(
+    "method_name,args",
+    [
+        ("get_api_resources", ("group", "version")),
+        ("get_cluster_custom_object", ("group", "version", "plural", "name")),
+        ("get_cluster_custom_object_scale", ("group", "version", "plural", "name")),
+        ("get_cluster_custom_object_status", ("group", "version", "plural", "name")),
+        (
+            "get_namespaced_custom_object",
+            ("group", "version", "namespace", "plural", "name"),
+        ),
+        (
+            "get_namespaced_custom_object_scale",
+            ("group", "version", "namespace", "plural", "name"),
+        ),
+        (
+            "get_namespaced_custom_object_status",
+            ("group", "version", "namespace", "plural", "name"),
+        ),
+    ],
+)
+def test_custom_objects_read_methods_skip_pretty(
+    mocker: MockerFixture, method_name: str, args: tuple[str, ...]
+) -> None:
+    # These generated read methods raise ApiTypeError if `pretty` is forwarded.
+    mocked_method = mocker.patch(f"kubernetes.client.CustomObjectsApi.{method_name}")
+
+    kubernetes_helper = wiremind_kubernetes.kubernetes_helper.KubernetesHelper(
+        dry_run=True, should_load_kubernetes_config=False
+    )
+
+    getattr(kubernetes_helper.client_custom_objects_api, method_name)(*args)
+
+    mocked_method.assert_called_once_with(*args)
+
+
+def test_custom_objects_list_methods_keep_pretty(mocker: MockerFixture) -> None:
+    # Keep the shared pretty behavior on list methods that still accept it.
+    mocked_list_cluster_custom_object = mocker.patch(
+        "kubernetes.client.CustomObjectsApi.list_cluster_custom_object"
+    )
+
+    kubernetes_helper = wiremind_kubernetes.kubernetes_helper.KubernetesHelper(
+        dry_run=True, should_load_kubernetes_config=False
+    )
+
+    kubernetes_helper.client_custom_objects_api.list_cluster_custom_object(
+        "group", "version", "plural"
+    )
+
+    mocked_list_cluster_custom_object.assert_called_once_with(
+        "group", "version", "plural", pretty=True
+    )
