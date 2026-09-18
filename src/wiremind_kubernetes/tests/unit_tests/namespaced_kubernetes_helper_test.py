b155c210f8d8ad0503241eafda2db73ddca1b541
Harsh Raj Singhania <harshrajsinghania@proton.me>
2026-09-18 19:35:38 +0530

    test: treat Succeeded pods as stopped in is_deployment_stopped

--- src/wiremind_kubernetes/tests/unit_tests/namespaced_kubernetes_helper_test.py
+++ src/wiremind_kubernetes/tests/unit_tests/namespaced_kubernetes_helper_test.py
@@ -28,3 +28,30 @@ def test_is_deployment_stopped_ignores_failed(mocker: MockerFixture) -> None:
     )
 
     assert namespaced_kubernetes_helper.is_deployment_stopped("bar")
+
+
+def test_is_deployment_stopped_ignores_succeeded(mocker: MockerFixture) -> None:
+    """
+    Test that we don't consider Succeeded (Completed) Pods as living Pods
+    """
+    mocker.patch("kubernetes.client.AppsV1Api")
+    mocker.patch("kubernetes.client.CoreV1Api")
+    mocker.patch("kubernetes.client.BatchV1Api")
+    mocker.patch("kubernetes.client.CustomObjectsApi")
+
+    class DummyStatusObject:
+        phase = "Succeeded"
+
+    class DummyPodObject:
+        status = DummyStatusObject()
+
+    namespaced_kubernetes_helper = wiremind_kubernetes.NamespacedKubernetesHelper(
+        should_load_kubernetes_config=False, namespace="foo"
+    )
+
+    mocker.patch(
+        "wiremind_kubernetes.NamespacedKubernetesHelper._get_pods_from_deployment",
+        return_value=[DummyPodObject()],
+    )
+
+    assert namespaced_kubernetes_helper.is_deployment_stopped("bar")
